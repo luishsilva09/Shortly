23fba063c0f71378d5305c679c0df55bc2807a55
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-04 16:37:59 -0300

    validar e salvar dados para adicionar url

--- src/controllers/addDeleteUrlController.js
+++ src/controllers/addDeleteUrlController.js
@@ -2,11 +2,17 @@ import connection from "../dbStrategy/postgres.js";
 import { nanoid } from 'nanoid'
 export async function addShortUrl(req,res){
     try{
+        const {rows: session} = res.locals.session
         let {url}= req.body
         url = await nanoid(8)
-        console.log(url)
-        console.log(token)
-        res.send("post")
+       await connection.query(`INSERT INTO shortlys("shortUrl","url","userId","visitCount") VALUES ($1,$2,$3,$4)`,
+        [url,req.body.url,session[0].userId,0])
+
+        const body = {
+            shortUrl: url
+        }
+
+        res.status(201).send(body)
     }catch (error) {
         console.log(error);
         res.sendStatus(500);
--- src/middleware/addShortValid.js
+++ src/middleware/addShortValid.js
@@ -1,13 +1,21 @@
-import joi from 'joi'
+import joi from 'joi';
+import connection from '../dbStrategy/postgres.js';
 import { urlSchema } from '../schemas/urlSchema.js';
+
 export async function addShortValid(req,res,next){
     const { authorization } = req.headers;
     const url = req.body
     const token = authorization?.replace('Bearer ', '');
-
     const {error} = urlSchema.validate(url)
+
+    const session = await connection.query(`SELECT * FROM sessions WHERE sessions.token = $1`,[token])
+
     if(error){
-        console.log(error)
         return res.sendStatus(422)
     }
+    if(!token || session.rowCount === 0){
+        return res.sendStatus(401)
+    }
+    res.locals.session = session
+    next()
 }
