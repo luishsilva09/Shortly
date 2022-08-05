87ac56ec81db4cc353c6cd395e0b24ebcc020228
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-04 21:15:36 -0300

    redirecionar usuario para a url

--- src/controllers/getUrlsController.js
+++ src/controllers/getUrlsController.js
@@ -13,7 +13,6 @@ export async function listUrl(req,res){
             shortUrl: shortData[0].shortUrl,
             url:shortData[0].url
         }
-        
         res.status(200).send(body)
     }catch (error) {
         console.log(error);
@@ -23,8 +22,15 @@ export async function listUrl(req,res){
 
 export async function openUrl(req,res){
     try{
-        
-        res.send("abrir urls")
+        const shortUrl = req.params.shortUrl;
+        const data= await connection.query(`SELECT * FROM shortlys WHERE shortlys."shortUrl" = $1`,[shortUrl])
+        if(data.rowCount === 0){
+            return res.sendStatus(404)
+        }
+        await connection.query(`UPDATE shortlys SET "visitCount" = $1 WHERE shortlys."shortUrl" = $2`,
+        [(data.rows[0].visitCount + 1), shortUrl])
+        res.send(data.rows[0])
+        //res.redirect(data.rows[0].url)
     }catch (error) {
         console.log(error);
         res.sendStatus(500);
