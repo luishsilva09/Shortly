4285a3f7cf387d13dd0f4f0f3c25adea01fb30fd
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-08 12:24:46 -0300

    ajuste de retorno da rota ranking

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -3,7 +3,20 @@ import { rankingRepository } from "../repository/rankingRepository.js";
 export async function ranking(req,res){
     try{
         const data = await rankingRepository.rankingData()
-        res.status(200).send(data.rows)
+        let ranking = []
+        data.rows.map(e => {
+            if(e.visitCount === null){
+               ranking.push({
+                id: e.id,
+                name : e.name,
+                linksCount: e.linksCount,
+                visitCount: 0,
+            })     
+            }else{
+                ranking.push(e)
+            } 
+        })
+        res.status(200).send(ranking)
     }catch (error) {
         console.log(error);
         res.sendStatus(500);
