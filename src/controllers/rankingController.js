9682517aacac14c2aa350b543297d8f33fa4184d
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-08 17:06:55 -0300

    ajuste forma de retorno do ranking

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -3,20 +3,7 @@ import { rankingRepository } from "../repository/rankingRepository.js";
 export async function ranking(req,res){
     try{
         const data = await rankingRepository.rankingData()
-        let ranking = []
-        data.rows.map(e => {
-            if(e.visitCount === null){
-               ranking.push({
-                id: e.id,
-                name : e.name,
-                linksCount: e.linksCount,
-                visitCount: 0,
-            })     
-            }else{
-                ranking.push(e)
-            } 
-        })
-        res.status(200).send(ranking)
+        res.status(200).send(data.rows)
     }catch (error) {
         console.log(error);
         res.sendStatus(500);
--- src/repository/rankingRepository.js
+++ src/repository/rankingRepository.js
@@ -2,10 +2,10 @@ import connection from "../dbStrategy/postgres.js";
 
 async function rankingData(){
     return await connection.query(`
-    SELECT users.id AS id, users.name AS name, COUNT(shortlys)::int AS "linksCount", SUM(shortlys."visitCount")::int AS "visitCount" FROM users 
+    SELECT users.id AS id, users.name AS name, COUNT(shortlys)::int AS "linksCount", COALESCE(SUM(shortlys."visitCount"),0)::int AS "visitCount" FROM users 
     LEFT JOIN shortlys ON shortlys."userId" = users.id
     GROUP BY users.id 
-    ORDER BY "visitCount" ASC
+    ORDER BY COALESCE(SUM(shortlys."visitCount"),0) DESC
     LIMIT 10
     `)
 }
